9817aca18bef582f99a1c371ba0750d0eb039ae1
DanielJDufour <daniel.j.dufour@gmail.com>
2022-02-25 11:31:01 -0500

    added class_properties_delimiter

--- test.js
+++ test.js
@@ -6,6 +6,12 @@ const { calculate } = require("./zonal.js");
 
 const loadJSON = fp => JSON.parse(fs.readFileSync(fp, "utf-8"));
 
+// test buffers file created by running the following
+// require("fs").writeFileSync("earthquake_buffers.geojson", JSON.stringify(require("@turf/buffer").default(JSON.parse(require("fs").readFileSync("./wld_nhrpub_adameqepic14days_wfp.geojson", "utf-8")), 100)));
+const earthquake_bufers = loadJSON("./data/earthquake_buffers.geojson");
+
+const indonesia_admin_boundaries = loadJSON("./data/admin_idn.json");
+
 const nam_admin2 = loadJSON("./data/nam_admin2.json");
 const wind_buffers = loadJSON(
   "./data/wld_nhr_adamtsbufferscurrent_wfp.geojson"
@@ -31,6 +37,69 @@ const windhoek = {
 function saveAsCSV(filepath, rows) {
   fs.writeFileSync(filepath, papaparse.unparse(rows, { quotes: true }));
 }
+
+test("earthquake buffers", ({ eq }) => {
+  const results = calculate({
+    debug_level: 2,
+    zones: indonesia_admin_boundaries,
+    zone_properties: ["A2NAME"],
+    classes: earthquake_bufers,
+    class_properties: ["mag"],
+    preserve_features: true
+  });
+  const jayapura_rows = results.table.filter(
+    row => row["zone:A2NAME"] === "JAYAPURA"
+  );
+
+  // there are 2 buffers close to each other
+  // with 5 mag and 5.5 mag
+  eq(jayapura_rows, [
+    {
+      "zone:A2NAME": "JAYAPURA",
+      "class:mag": 5.5,
+      "stat:area": 10108102039,
+      "stat:percentage": 0.6996874816842877
+    },
+    {
+      "zone:A2NAME": "JAYAPURA",
+      "class:mag": 5,
+      "stat:area": 9000877103,
+      "stat:percentage": 0.623044861325013
+    },
+    {
+      "zone:A2NAME": "JAYAPURA",
+      "class:mag": null,
+      "stat:area": 4338493485,
+      "stat:percentage": 0.3003125185233737
+    }
+  ]);
+
+  const jayapura_props = results.geojson.features.find(
+    f => f.properties["A2NAME"] === "JAYAPURA"
+  ).properties;
+  eq(jayapura_props["zonal:stat:minority"], 5);
+  eq(jayapura_props["zonal:stat:majority"], 5.5);
+  eq(jayapura_props, {
+    A1NAME: "PAPUA",
+    A1CODE: 94,
+    NSO_CODE: 9403,
+    A2NAME: "JAYAPURA",
+    TYPE: "Kabupaten",
+    A2TEXT: "9403",
+    Hectares: 1439000,
+    "zonal:zone_id": ["JAYAPURA"],
+    "zonal:stat:area": 14446595521,
+    "zonal:stat:minority": 5,
+    "zonal:stat:majority": 5.5,
+    "zonal:stat:percentage": 0.6996874814766263,
+    "zonal:stat:sum": 10108102036,
+    "zonal:stat:classes": {
+      5: { area: 9000877103, percentage: 0.623044861325013 },
+      5.5: { area: 10108102039, percentage: 0.6996874816842877 },
+      null: { area: 4338493485, percentage: 0.3003125185233737 }
+    }
+  });
+});
 
 test("polygon zones and point class", ({ eq }) => {
   const result = calculate({
@@ -188,6 +257,26 @@ test("admin boundaries with wind cones", ({ eq }) => {
   });
   const feature = results.geojson.features[0];
   eq(feature.properties, {
+    OBJECTID: 385,
+    ParishName: "Acadia",
+    ParishNumber: "01",
+    DMV_ParishCode: "ACD",
+    DesignatedCulturalArea: "Acadiana",
+    TourismRegionCode: 3,
+    TourismRegionName: "Cajun Country",
+    ParishSeatName: "Crowley",
+    DOTD_DistrictNumber: "03",
+    DOTD_DistrictName: "District 03",
+    created_user: null,
+    created_date: null,
+    created_date_UTCOffset: null,
+    last_edited_user: "CRNH0007",
+    last_edited_date: 1486841361000,
+    last_edited_date_UTCOffset: "-06:00",
+    service_last_updated: 1558289999000,
+    service_last_updated_UTCOffset: "-05:00",
+    GlobalID: "{7DBC1C81-D0D8-45EB-9A72-4CFCA3E7A246}",
+    ParishFIPS: "001",
     "zonal:zone_id": ["Acadia"],
     "zonal:stat:area": 1708349991,
     "zonal:stat:minority": "60 km/h",
@@ -195,14 +284,8 @@ test("admin boundaries with wind cones", ({ eq }) => {
     "zonal:stat:percentage": 1,
     "zonal:stat:sum": 1708349991,
     "zonal:stat:classes": {
-      "60 km/h": {
-        area: 1708349991,
-        percentage: 1
-      },
-      null: {
-        area: 0,
-        percentage: 0
-      }
+      "60 km/h": { area: 1708349991, percentage: 1 },
+      null: { area: 0, percentage: 0 }
     }
   });
 });
